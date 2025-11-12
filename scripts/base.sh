f49b1bd3ca6b828494c23ffdb5e3a44b490a7bff
valvarezg1974 <victor_alv2005@hotmail.com>
2025-11-12 12:21:40 +0100

    change public key

--- scripts/base.sh
+++ scripts/base.sh
@@ -2,10 +2,8 @@
 
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 yum -y install gcc make gcc-c++ kernel-devel-`uname -r` perl grub2-tools net-tools
+yum -y install ncdu tldr lsof rsync
 yum -y update
-#yum -y install epel-release.noarch
-#rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
-#yum -y install httpd* php mariadb*
 sudo dnf config-manager --set-enabled crb
 sudo dnf makecache
 sudo dnf groupinstall -y "Development Tools"
